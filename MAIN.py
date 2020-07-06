eb4235451b21a05d0a08996e9616eda528d6ffd2
emberrett <berrettethan@gmail.com>
2020-07-05 21:41:41 -0600

    Converted script to handle JSON files rather than HTML. Much faster thankfully and requires much less code. For the time being timestamps are in Unix format; added placeholder timestamps for start date and end date. Added prompt for what to name the group as it shows up in the plot; the group name is not provided in the JSON file, so may have to use the name of the file itself as group name.

--- MAIN.py
+++ MAIN.py
@@ -1,5 +1,3 @@
-# library for handling html
-from bs4 import BeautifulSoup as bs
 # library for handling dates and time
 from datetime import datetime
 # library for plotting data
@@ -9,76 +7,59 @@ import operator
 # library for UI
 from tkinter import filedialog
 from tkinter import *
+# library for handling json
+import json
 
 # keeps root window closed
 root = Tk()
 root.withdraw()
 
-# choose file to run queries on
-# file = "/home/ethan/Desktop/facebook-ethanberrett23/messages/inbox/2016marchmadness_6aeoefufua/message_1.html"
+# ask user to select file
 file = filedialog.askopenfilename()
 
-# pass text from html file into string
-with open(file, 'r') as f:
-    html_string = f.read()
-
-# parse HTML string with beautifulsoup
-parsed_file = str(bs(html_string, 'html.parser'))
-
-start_date = '2010-03-17 10:55:00'
-end_date = '2020-03-17 10:56:00'
+start_date = 1593578720
+end_date = 1585102129512
 
 
 # write a function that takes the file name as a parameter
-def get_messages(html_file):
-    # div class that always precedes a message
-    div_class = "_3-96 _2pio _2lek _2lel"
-
-    # skip first message since it isn't an actual message
-    messages = re.finditer(div_class, html_file)
-    next(messages)
+def get_messages():
+    # open json file in read mode
+    with open(file, "r") as read_file:
+        data = json.load(read_file)
 
     # create dictionary to store messages
     mess_dict = {}
     # starting number for dictionary
     x = 0
-    for m in messages:
-        # find date of message
-        date_end = m.start() - 79
-        cut_date = parsed_file[:date_end]
-        date_start = cut_date.rfind("2lem") + 6
-        date_time = cut_date[date_start:]
+    for message in data['messages']:
+        # if message is a generic textual message
+        if 'content' in message:
+            # find content of message
+            message_content = message['content']
 
-        # convert to standard date time
-        date_time = datetime.strptime(date_time, "%b %d, %Y, %I:%M %p")
-        date_time = str(date_time)
+            # find time of message
+            message_timestamp = message['timestamp_ms']
 
-        # find name of message sender
-        name_start = m.start() + 25
-        cut_name = parsed_file[name_start:]
-        name_end = cut_name.find("</div>")
-        name = cut_name[0:name_end]
+            # find message sender
+            message_sender = message['sender_name']
 
-        # find message
-        cut_message = cut_name[name_end + 52:]
-        message_end = cut_message.find("</div>")
-        message = cut_message[:message_end]
-
-        message_tuple = date_time, name, message
+        message_tuple = message_timestamp, message_sender, message_content
         mess_dict[x] = message_tuple
         x += 1
+
     return mess_dict
 
 
 def find_sender_count():
-    mess_dict = get_messages(parsed_file)
+    mess_dict = get_messages()
+    print(mess_dict)
     name_list = {}
     # get unique names in messages
     for x in mess_dict:
         y = mess_dict[x][1]
         if y not in name_list:
             name_list[y] = 0
-        # check if message is in date range
+            # check if message is in date range
         if start_date <= mess_dict[x][0] <= end_date:
             name_list[y] += 1
     print(name_list)
@@ -86,12 +67,11 @@ def find_sender_count():
     # sort dictionary by descending order
     name_list = dict(sorted(name_list.items(), key=operator.itemgetter(1), reverse=True))
 
-    print(parsed_file.title())
-
-    chart_title = re.search('<title>(.*)</title>', parsed_file)
-    chart_title = (chart_title.group(1))
-    chart_title = "Number of Messages Sent in " + chart_title + '\n' + " from " + start_date.split(' ')[0] + " to " + \
-                  end_date.split(' ')[0]
+    group_name = input("What is the name of this group? ")
+    string_start_date = str(start_date)
+    string_end_date = str(end_date)
+    chart_title = "Number of Messages Sent in " + group_name + '\n' + " from " + str(start_date) + " to " + \
+                  str(end_date)
     # set x axis
     keys = name_list.keys()
     # set y axis
@@ -111,4 +91,3 @@ def find_sender_count():
 # add all unique values to tuples
 
 find_sender_count()
-
